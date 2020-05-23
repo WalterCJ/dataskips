a38d1a0c6e06d5333231bb6f868f5e31ca3bc6ee
WalterCJ <waltercavalcantejunior@gmail.com>
2020-05-23 20:24:40 -0300

    add design and containers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
+import Layout from "./components/Layout";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-      <div className="ml-6 pt-1">
-        <h1 className="text-2xl text-blue-700 leading-tight">
-          Tailwind and Create React App
-        </h1>
-        <p className="text-base text-gray-700 leading-normal">Test</p>
-      </div>
-    </div>
+    <Layout>
+      <Sidebar />
+      <Main />
+    </Layout>
   );
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { ReactComponent as Image } from "../assets/image.svg";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+
+export default function Sidebar() {
+  return (
+    <div className="justify-center p-12 md:p-5 md:w-7/12 bg-white">
+      <div className="md:w-full lg:w-5/6 xl:max-w-sm mx-auto hidden md:block">
+        <Image />
+      </div>
+      <div className="flex items-center md:justify-center mt-4">
+        <span className="h-10 w-10 md:h-8 md:w-8">
+          <Logo />
+        </span>
+        <span className="font-bold text-2xl md:text-2xl lg:text-3xl text-red-500 pl-4">
+          DataSkips
+        </span>
+      </div>
+      <div className="pl-0">
+        <h1 className="font-light text-lg mt-6 md:text-center">
+          Transform your tabular data!
+        </h1>
+        <p className="text-base md:text-base text-gray-600 mt-3 leading-relaxed md:text-center">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
+          veritatis quo quas. Dignissimos accusantium aliquid, dolor numquam
+          eveniet error quae harum? Debitis officia rem atque fugit praesentium
+          autem ullam recusandae?
+        </p>
+      </div>
+    </div>
+  );
+}
